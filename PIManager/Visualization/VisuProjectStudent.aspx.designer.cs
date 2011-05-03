28fb826735b11fe4163f7a280305ff5d8ef00f12
Lauriane Savary <lauriane.savary@gmail.com>
2011-05-03 15:20:59 +0200

    Resolved problem with links for subscription and unsubscription.

--- PIManager/Visualization/VisuProjectStudent.aspx.designer.cs
+++ PIManager/Visualization/VisuProjectStudent.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace PIManager.Visualization {
         protected global::System.Web.UI.WebControls.Label errorLabel;
         
         /// <summary>
+        /// msgLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label msgLabel;
+        
+        /// <summary>
         /// ProjectTable control.
         /// </summary>
         /// <remarks>
